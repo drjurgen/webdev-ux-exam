c2b911dda9afb2d89a492b7e140adefebc03d17a
Alexander <alex143r@stud.kea.dk>
2021-12-10 22:10:00 +0100

    "test"

---
+++ js/schedule-tabs.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", initSchedule);
+elem = document.querySelector(".tabs");
+options = {};
+function initSchedule() {
+  if (window.innerWidth < 993) {
+    instance = M.Tabs.init(elem, options);
+  }
+  window.onresize = reportWindowSize;
+}
+
+function reportWindowSize() {
+  if (window.innerWidth > 993) {
+    if (instance._indicator.parentNode != null) {
+      instance.destroy();
+    }
+  } else {
+    instance = M.Tabs.init(elem, options);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
-window.addEventListener("load", initSchedule);
-elem = document.querySelector(".tabs");
-options = {};
-function initSchedule() {
-  console.log(window.innerHeight);
-  if (window.innerWidth < 993) {
-    instance = M.Tabs.init(elem, options);
-  }
-  window.onresize = reportWindowSize;
-}
-
-function reportWindowSize() {
-  if (window.innerWidth > 993) {
-    if (instance._indicator.parentNode != null) {
-      instance.destroy();
-    }
-  } else {
-    instance = M.Tabs.init(elem, options);
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var options = {
+    fullWidth: false,
+    duration: 400,
+    dist: 0,
+    padding: -100,
+    numVisible: 5,
+  };
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
