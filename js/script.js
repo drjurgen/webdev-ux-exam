d7648bc4852d21357ee7483e9337db0a07b56257
Alexander Tyllesen <alex143r@stud.kea.dk>
2021-12-12 17:22:49 +0100

    "test"

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var options = {
-    fullWidth: false,
-    duration: 400,
-    dist: 0,
-    padding: -100,
-    numVisible: 5,
-  };
-  var elems = document.querySelectorAll(".carousel");
-  var instances = M.Carousel.init(elems, options);
-});
+window.addEventListener("DOMContentLoaded", initAssignments);
+const elem = document.querySelector(".carousel");
+const options = {
+  fullWidth: false,
+  duration: 400,
+  dist: 0,
+  padding: -100,
+  numVisible: 5,
+};
+let instance;
+function initAssignments() {
+  if (window.innerWidth < 2000) {
+    instance = M.Carousel.init(elem, options);
+  }
+  window.onresize = reportWindowSize;
+}
+
+function reportWindowSize() {
+  console.log(instance);
+
+  if (window.innerWidth > 993) {
+    if (instance != undefined) {
+      instance.destroy();
+    }
+  } else {
+    instance = M.Carousel.init(elem, options);
+  }
+}
